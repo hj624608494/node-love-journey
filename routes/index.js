db5f0b6c2cde68ccc1ab271a01943ccad618764f
hanjie <jie_han@uyoung.com.cn>
2016-02-19 17:34:26 +0800

    引入 ejs 模板引擎

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var listDatas = [1,2,3,4,5];
+  res.render('test', { title: 'ejs' , list: listDatas});
 });
 
 

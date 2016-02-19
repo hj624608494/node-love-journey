dd6bcc02a0054e842c3e00cb790b7e2977c16753
hanjie <jie_han@uyoung.com.cn>
2016-02-19 15:06:58 +0800

    node request

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,22 @@ router.get('/index/waist', function(req, res, next) {
   res.render('index/waist', { title: '商品' });
 });
 
+/**
+ * login
+ */
+router.post('/index/login', function(req, res, next) {
+	var postData = req.body;
+	if (postData.username == '123' && postData.password == '123') {
+		res.send({
+			ret: true
+		})
+	}else{
+		res.send({
+			ret: false
+		})
+	}
+	// console.log(res);
+	// console.log(next);
+});
+
 module.exports = router;
